f2a9c18e183af593a1674694e89f1d9d3eef8cd4
eelrb <eelrb@yahoo.com>
2019-04-23 11:59:59 -0700

    change instance to micro

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "aws_ami" "my_image" {
 
 resource "aws_instance" "blee-vault" {
   ami           = "${data.aws_ami.my_image.id}"
-  instance_type = "t2.large"
+  instance_type = "t2.micro"
   #vpc_security_group_ids = ["${aws_security_group.main_sec_group.id}"]
   #subnet_id = "${aws_subnet.subnet1.id}"
   key_name = "blee-keypair"
